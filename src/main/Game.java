51bc8dccf174eee19974cbfeb3bd8decfe4f43ba
Sahetiya, Rahul Sunil <rsahetiya3@gatech.edu>
2021-02-28 06:31:30 -0500

    Adds some styling for room scene, still experimenting with things though

--- src/main/Game.java
+++ src/main/Game.java
@@ -16,6 +16,7 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -41,43 +42,70 @@ public class Game extends Application {
         buttonStart.setOnAction(e -> primaryStage.setScene(configurationScene));
         Button buttonEnterDungeon = new Button("Enter the Dungeon");
         buttonEnterDungeon.setOnAction(e -> primaryStage.setScene(RoomScene));
-        //adds buttonss
+        //adds buttons
         welcomeScreenL.getChildren().add(buttonStart);
         configurationScreenL.getChildren().add(buttonEnterDungeon);
 
         //Room Scene Set Up
+        //Borders of the border pane will be Vbox's that house buttons
+        VBox leftSide = new VBox();
+        VBox rightSide = new VBox();
+        VBox upSide = new VBox();
+        VBox downSide = new VBox();
+        leftSide.setStyle("-fx-background-color: #000000;");
+        rightSide.setStyle("-fx-background-color: #000000;");
+        upSide.setStyle("-fx-background-color: #000000;");
+        downSide.setStyle("-fx-background-color: #000000;");
+        //Making the buttons and styling
         Button buttonUp = new Button("Up");
         Button buttonDown = new Button("Down");
         Button buttonRight = new Button("Right");
         Button buttonLeft = new Button("Left");
+        buttonUp.setFont(Font.font("Cambria", 32));
+        buttonUp.setPrefWidth(128);
+        buttonUp.setPrefHeight(72);
+        buttonDown.setFont(Font.font("Cambria", 32));
+        buttonDown.setPrefWidth(128);
+        buttonDown.setPrefHeight(72);
+        buttonRight.setFont(Font.font("Cambria", 32));
+        buttonRight.setPrefHeight(128);
+        buttonRight.setPrefWidth(72);
+        buttonLeft.setFont(Font.font("Cambria", 32));
+        buttonLeft.setPrefHeight(128);
+        buttonLeft.setPrefWidth(72);
         int startingMoney = 0;
         Label moneyStatus = new Label("Money: " + startingMoney);
+        moneyStatus.setFont(Font.font("Cambria", 32));
         moneyStatus.setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 255, 0.7),
-                new CornerRadii(5.0), new Insets(-5.0))));
-        //middle view in screen
+                new CornerRadii(0.0), new Insets(0.0))));
+        //middle view in screen (background and money, basically the main screen that the player looks at)
         StackPane MainView = new StackPane();
         MainView.setAlignment(moneyStatus, Pos.TOP_RIGHT);
         Image background = new Image("file:src/dungeonTest.jpg");
         BackgroundImage dungeonBackground = new BackgroundImage(background,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.REPEAT,
+                BackgroundRepeat.REPEAT,
                 BackgroundPosition.CENTER,
-                new BackgroundSize(1000,
-                        700,
+                new BackgroundSize(1200,
+                        450,
                         false,
                         false,
                         true,
-                        true));
+                        false));
         MainView.setBackground(new Background(dungeonBackground));
         MainView.getChildren().add(moneyStatus);
-        roomL.setAlignment(buttonUp, Pos.CENTER);
-        roomL.setAlignment(buttonDown, Pos.CENTER);
-        roomL.setAlignment(buttonLeft, Pos.CENTER);
-        roomL.setAlignment(buttonRight, Pos.CENTER);
-        roomL.setTop(buttonUp);
-        roomL.setBottom(buttonDown);
-        roomL.setRight(buttonRight);
-        roomL.setLeft(buttonLeft);
+        upSide.getChildren().add(buttonUp);
+        downSide.getChildren().add(buttonDown);
+        rightSide.getChildren().add(buttonRight);
+        leftSide.getChildren().add(buttonLeft);
+        upSide.setAlignment(Pos.CENTER);
+        downSide.setAlignment(Pos.CENTER);
+        leftSide.setAlignment(Pos.CENTER);
+        rightSide.setAlignment(Pos.CENTER);
+        roomL.setTop(upSide);
+        roomL.setBottom(downSide);
+        roomL.setRight(rightSide);
+        roomL.setLeft(leftSide);
         roomL.setCenter(MainView);
         primaryStage.setScene(welcomeScene);
         primaryStage.show();
